ffc5e7905959e87c32bb65b646152edadaf0753e
Aaron Young <mustarddevelopment@gmail.com>
2018-06-06 20:51:09 -0700

    Updating the basic structure. pre commit before ejecting for css modules.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
-import registerServiceWorker from './util/registerServiceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import registerServiceWorker from "./util/registerServiceWorker";
 
 render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 registerServiceWorker();
