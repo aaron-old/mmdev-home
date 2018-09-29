c63f01ff866e300607d2e58494188833a34bb3aa
Aaron Young <mustarddevelopment@gmail.com>
2018-09-29 09:40:52 -0700

    moving some of the components around. Since these will be full site migrations, doing a partial push of content.

---
+++ new/src/components/footer/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import footerStyles from './footer.module.scss';
+import layoutStyles from '../../layout.module.scss';
+
+const Footer = () => {
+    return (
+        <footer className={footerStyles.footer}>
+            <section className={footerStyles.section}>
+                footer
+            </section>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ new/src/components/layout/layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../footer/footer';
+import Header from '../header/header';
+
+const Layout = ({children}) => {
+    return(
+        <>
+        <Header></Header>
+        <Footer></Footer>
+        </>
+        
+    )
+}
+export default Layout;
